3ef0eab6c9fa73080273b3118e25374f331eabd2
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-26 13:01:21 +0300

    Working on 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+
+
+export class App extends Component {
+  state = {
+    page: 1,
+    query: "",
+    items: [],
+  };
+
+
+  render() {
+    return (
+      <header class="searchbar">
+        <form class="form">
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//! OLD ------------------------------------
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
