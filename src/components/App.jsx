741fca965f850f2969c4ba70ffba29df0ba488fe
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-26 13:32:12 +0300

    Working on 1 url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,36 @@ import { Component } from 'react';
 export class App extends Component {
   state = {
     page: 1,
-    query: "",
+    query: "cat",
     items: [],
   };
 
+  //! Формируем строку URL-запроса:
+  API_KEY = '28759369-3882e1068ac26fe18d14affeb';
+  BASE_URL = 'https://pixabay.com/api/';
+  per_page = 12
+  url = `${this.BASE_URL}?key=${this.API_KEY}&q=${this.state.query}&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=${this.per_page}`; //! with API_KEY
+  
 
   render() {
+    console.log(this.url);
+
+
+
+
+
     return (
-      <header class="searchbar">
-        <form class="form">
-          <button type="submit" class="button">
-            <span class="button-label">Search</span>
+      <header className="searchbar">
+        <form className="form">
+          <button
+            type="submit"
+            className="button"
+          >
+            <span className="button-label">Search</span>
           </button>
 
           <input
-            class="input"
+            className="input"
             type="text"
             autocomplete="off"
             autofocus
