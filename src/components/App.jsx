3806247a53ca938f087ca983b2dae81a9455e4a2
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-26 17:33:21 +0300

    Working on 4 fetch & submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,10 @@ import axios from 'axios';
 export class App extends Component {
   state = {
     page: 1,
-    query: "cat",
+    query: '',
     hits: [],
-    loading: false,
+    isLoading: false,
+    error: false,
   };
 
 
@@ -17,42 +18,121 @@ export class App extends Component {
   API_KEY = '28759369-3882e1068ac26fe18d14affeb';
   BASE_URL = 'https://pixabay.com/api/';
   per_page = 12;
-  url = `${this.BASE_URL}?key=${this.API_KEY}&q=${this.state.query}&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=${this.per_page}`; //! with API_KEY
+  // url = `${this.BASE_URL}?key=${this.API_KEY}&q=${this.state.query}&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=${this.per_page}`; //! with API_KEY
   
 
 
 //* ================================ МЕТОДЫ ==========================================================
   //! axios.get-запрос: (с async/await)
-  async fetchHits(url) {
+  async fetchHits() {
+    console.log(this.state.query); //!
+    const url = `${this.BASE_URL}?key=${this.API_KEY}&q=${this.state.query}&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=${this.per_page}`; 
+    console.log("fetchHits: ", url); //!
     const response = await axios.get(url);
-    const newHits = await response.data;
-    // console.log("newHits: ", newHits); //!
-    // const { totalHits, hits } = newHits; //?
-    const { hits } = newHits;
-    // const all = { totalHits, hits } //?
-    return hits;
+    // const newHits = await response.data;
+    // const { hits } = newHits;
+    // console.log(hits); //!
+    // return hits;
+    // console.log(response.data); //!
+    return response.data;
   };
   
-  
-  componentDidMount() {
-    this.setState({ loading: true })
-    setTimeout(() => {
-      this.fetchHits(this.url)
-        .then(( hits ) => {
-            // console.log("totalHits: ", totalHits); //!
+
+    // async componentDidMount() {
+  //   try {
+  //   this.setState({ isLoading: true })
+  //   // setTimeout(() => { //?
+  //     const { hits } = await this.fetchHits(this.url)
+  //     this.setState({ hits, isLoading: false });
+  //         console.log("fetch hits: ", hits); //!
+  //         console.log("fetch hits[0]: ", hits[0]); //!
+  //         console.log("fetch hits[0].id: ", hits[0].id); //!
+  //         console.log("fetch hits[0].webformatURL: ", hits[0].webformatURL); //!
+  //       console.log("fetch hits[0].largeImageURL: ", hits[0].largeImageURL); //!
+  //   } catch (error) {
+  //     this.setState({ error: true, isLoading: false });
+  //     console.log(error);
+  //     }
+  //       // return hits //?
+  //       // }) //?
+  //       // .then(hits => this.setState({ hits }))  //?
+  //       // .finally(() => this.setState({ isLoading: false })); //?
+  //   // }, 2000); //?
+  // }
+
+
+
+  // handleChange = event => {
+  //   // console.log(event.currentTarget); //!
+  //   // console.log(event.currentTarget.name); //!
+  //   console.log(event.currentTarget.value); //!
+
+  //   // this.setState({ name: event.currentTarget.value }); //?
+  //   // this.setState({ [event.currentTarget.name]: event.currentTarget.value }); //?
+
+  //   const { value } = event.currentTarget;
+
+  //   // if (value.trim() === '') {
+  //   //   console.log(value);
+  //   //   // alert('Введите имя');
+  //   //   toast.error('Введите имя'); //? РАБОТАЕТ, но НЕ ПРАВИЛЬНО!!!
+  //   //   return;
+  //   // }
+
+  //     this.setState({ query: value });
+  // };
+
+
+  //! Очистка поля инпута
+  // reset = () => {
+  //   this.setState({ query: '' });
+  // };
+
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // console.log(event.target.elements.query.value); //!
+    this.setState ({
+      page: 1,
+      query: event.target.elements.query.value,
+      hits: [],
+    });
+    event.target.reset()
+    // this.props.onSubmit(name, number);
+    // this.reset();
+  };
+
+
+
+
+  async componentDidUpdate(_, prevState) {
+    try { //?
+      console.log("prevState.page: ", prevState.page); //!
+      console.log("this.state.page: ", this.state.page); //!
+    
+      console.log("prevState.query: ", prevState.query); //!
+      console.log("this.state.query: ", this.state.query); //!
+      
+      if (
+        prevState.page !== this.state.page ||
+        prevState.query !== this.state.query
+      ) {
+        this.setState({ isLoading: true })
+        // console.log("componentDidUpdate: ", this.url); //!
+        const { hits } = await this.fetchHits()
+        this.setState({ hits, isLoading: false });
           console.log("fetch hits: ", hits); //!
           console.log("fetch hits[0]: ", hits[0]); //!
           console.log("fetch hits[0].id: ", hits[0].id); //!
           console.log("fetch hits[0].webformatURL: ", hits[0].webformatURL); //!
           console.log("fetch hits[0].largeImageURL: ", hits[0].largeImageURL); //!
-        return hits
-        })  
-        // .then(res => res.json()) //? old
-        // .then(console.log) //!
-        .then(hits => this.setState({ hits })) 
-        .finally(() => this.setState({ loading: false }));
-    }, 2000);
+      }
+    } catch (error) { //?
+      this.setState({ error: true, isLoading: false }); //?
+      console.log(error); //?
+      } //?
   }
+
 
 
 
@@ -70,18 +150,21 @@ export class App extends Component {
   render() {
     const { hits } = this.state
     // console.log(this.url); //!
-    console.log("hits: ", hits); //!
-    console.log("hits[0]: ", hits[0]); //!
+    // console.log("hits: ", hits); //!
+    // console.log("hits[0]: ", hits[0]); //!
     // console.log("hits[0].id: ", hits[0].id); //!
     
-    console.log("this.state.hits: ", this.state.hits); //!
-    console.log("this.state.hits[0]: ", this.state.hits[0]); //!
+    // console.log("this.state.hits: ", this.state.hits); //!
+    // console.log("this.state.hits[0]: ", this.state.hits[0]); //!
     // console.log("this.state.hits[0].id: ", this.state.hits[0].id); //!
 
     return (
       // <Searchbar/>
       <header className="searchbar">
-        <form className="form">
+        <form
+          className="form"
+          onSubmit={this.handleSubmit}
+        >
           <button
             type="submit"
             className="button"
@@ -92,9 +175,11 @@ export class App extends Component {
           <input
             className="input"
             type="text"
+            name = "query"
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            // onChange={this.handleChange}
           />
           <br />
           <button
