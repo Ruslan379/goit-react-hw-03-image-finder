d6c4271eb24e7ccbda03b03ea0eb51bb0bf9b901
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-29 12:34:52 +0300

    FINAL (Refactoring-1)

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,24 +5,12 @@ import { Audio } from  'react-loader-spinner'
 
 
 
-
-
 export const Loader = () => (
   <h1 style={{ margin: '0 auto' }}>
     Загружаем...
-      <Audio
-        // height = "80"
-        // // width = "80"
-        width = "100%"
-        // radius = "9"
-        // color = 'green'
-        // ariaLabel = 'three-dots-loading'     
-        // wrapperStyle
-        // wrapperClass
-      />
+      <Audio width = "100%" />
   </h1>
 );
-
 
 
 
